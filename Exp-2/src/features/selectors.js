84800dddeb50ba3bfd0601ac8ce55fc605b3333e
sidhantkumargupta14 <sidhantkumargupta14@gmail.com>
2026-07-30 10:48:20 +0530

    Delete Exp-2 directory

--- Exp-2/src/features/selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectCounter = (state) => state.counter.value;
-
-export const counterValue = createSelector(
-  [selectCounter],
-  (value) => value
-);
